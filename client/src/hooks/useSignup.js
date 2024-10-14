097790e18a601ec10f957714f433cc5de5cb3176
Shehan Gunasekara <87413135+Shehan-Gunasekara@users.noreply.github.com>
2024-10-14 21:55:48 +0530

    wdk na

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -1,40 +1,40 @@
-import {useState} from 'react'
-import { useAuthContext } from './useAuthContext'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
-    const navigate = useNavigate();
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const {dispatch} = useAuthContext()
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
 
-    const signup = async (name, email, password, confirmPassword) => {
-        setIsLoading(true)
-        setError(null)
+  const signup = async (name, email, password, confirmPassword) => {
+    setIsLoading(true);
+    setError(null);
 
-        const response = await fetch('/api/users/signup', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({name, email, password, confirmPassword})
-        })
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password, confirmPassword }),
+    });
 
-        const json = await response.json()
-        if(!response.ok){
-            setIsLoading(false)
-            setError(json.error)
-        }
-
-        if(response.ok){
-            // save the user to local staorage
-            localStorage.setItem('user', JSON.stringify(json))
-
-            // update the auth context
-            dispatch({type: 'LOGIN', payload: json})
-
-            setIsLoading(false)
-            navigate("/account");
-        }
+    const json = await response.json();
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
     }
 
-    return {signup, isLoading, error}
-}
+    if (response.ok) {
+      // save the user to local staorage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setIsLoading(false);
+      navigate("/account");
+    }
+  };
+
+  return { signup, isLoading, error };
+};
